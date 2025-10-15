b80f7f0d88294a85ed84f955d806d9e33fb9dbff
dziner <48778695+dziner@users.noreply.github.com>
2025-10-15 19:01:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: "/customer-doc/",
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
